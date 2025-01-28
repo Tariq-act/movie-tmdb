62912070197f2c8c6d9a3fb90dd22bb73fdb6be3
Tariq-act <khantarikact@gmail.com>
2025-01-28 23:16:42 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import "./App.css";
+import Search from "./components/Search.tsx";
+
+function App() {
+  return (
+    <main>
+      <div className={"pattern"} />
+
+      <div className={"wrapper"}>
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className={"text-gradient"}>Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+
+        <Search />
+      </div>
+    </main>
+  );
+}
+
+export default App;
